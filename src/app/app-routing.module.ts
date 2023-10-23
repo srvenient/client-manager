98015d985e37a0ecd13866eed6c1abe2a3e9c520
SrVenient <srvenient@gmail.com>
2023-10-23 16:51:14 -0500

    codestyle: the correct identification has been set

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,29 +2,29 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 import {ClientComponent} from "./components/client/client.component";
-import {RegisterClientComponent} from "./components/register-client/register-client.component";
-import {SearchClientComponent} from "./components/search-client/search-client.component";
+import {RegisterComponent} from "./components/client/register/register.component";
+import {SearchComponent} from "./components/client/search/search.component";
 
 /**
  * The routes array contains the routes of our application.
  */
 const routes: Routes = [
-  {path: 'clients', component: ClientComponent},
-  {path: '', redirectTo: 'clients', pathMatch: 'full'},
-  {path: 'clients/register-client', component: RegisterClientComponent},
-  {path: 'clients/:sharedKey', component: SearchClientComponent},
+    {path: 'clients', component: ClientComponent},
+    {path: '', redirectTo: 'clients', pathMatch: 'full'},
+    {path: 'clients/register', component: RegisterComponent},
+    {path: 'clients/search/:sharedKey', component: SearchComponent},
 ];
 
 /**
  * The AppRoutingModule is where we define the routes for our application.
  */
 @NgModule({
-  declarations: [],
-  exports: [RouterModule],
-  imports: [
-    CommonModule,
-    RouterModule.forRoot(routes)
-  ]
+    declarations: [],
+    exports: [RouterModule],
+    imports: [
+        CommonModule,
+        RouterModule.forRoot(routes)
+    ]
 })
 export class AppRoutingModule {
 }
--- src/app/client.service.spec.ts
+++ src/app/client.service.spec.ts
@@ -1,16 +1,16 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { ClientService } from './client.service';
+import {ClientService} from './client.service';
 
 describe('ClientService', () => {
-  let service: ClientService;
+    let service: ClientService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ClientService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(ClientService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
